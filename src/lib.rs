ff727eb74b2241e8697da39882c766726d263198
theredfish <did.julian@gmail.com>
2017-07-15 05:48:07 +0200

    Basic structure for the lib
    - Pattern matching
    - Search repositories with a given pattern
    - RepoManager with add function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
+extern crate glob;
+
+use glob::glob;
+
+// TODO separate code in files
+// TODO add Repository struct and impl
+
 pub struct Config {
     pub query: String,
-    pub subject: String,
+    pub path: String,
+}
+
+// TODO remove debug
+#[derive(Debug)]
+pub enum RepoManager {
+    Add(String),
+    /*List,
+    Rm (String),*/
 }
 
 impl Config {
@@ -14,7 +29,42 @@ impl Config {
 
         Ok(Config {
             query: query,
-            subject: subject,
+            path: subject,
         })
     }
+}
+
+impl RepoManager {
+    pub fn call(&self) {
+        match *self {
+            RepoManager::Add(ref path) => {
+                let git_pattern = String::from("/**/*.git");
+                let git_repo_list = search(&path_with_pattern(path, &git_pattern));
+                add(git_repo_list);
+            }
+
+        }
+    }
+}
+
+// TODO update repositories by a Vec<Repository>
+fn add(repositories: Vec<String>) {
+    println!("You're in the \"add\" fn");
+}
+
+// TODO return Vec<Repository>
+fn search(pattern_path: &String) -> Vec<String> {
+    let repositories: Vec<String> = Vec::new();
+
+    for entry in glob(pattern_path).unwrap() {
+        if let Ok(pattern_path) = entry {
+            println!("{:?}", pattern_path.display())
+        }
+    }
+
+    repositories
+}
+
+fn path_with_pattern(path: &String, pattern: &String) -> String {
+    path.to_string() + pattern
 }
