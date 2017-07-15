9c586d0eb3bd96218fec0700eebe3f844d466bb1
theredfish <did.julian@gmail.com>
2017-07-15 05:48:07 +0200

    Basic structure for the lib
    - Pattern matching
    - Search repositories with a given pattern
    - RepoManager with add function

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ extern crate grm;
 
 use std::env;
 use std::process;
+use std::path::Path;
 
 use grm::Config;
+use grm::RepoManager;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,5 +14,22 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Invoke {:?} on {:?}", config.query, config.subject);
+    // TODO check if the path exists before to continue
+
+    /*let path_exists = Path::new(config.path).exists();
+    match false {
+
+    }*/
+
+    run(config);
+}
+
+fn run(config: Config) {
+    match config.query.as_ref() {
+        "add" => RepoManager::Add(String::from(config.path)).call(),
+        _ => {
+            println!("{} : command not found", config.query);
+            process::exit(1);
+        },
+    }
 }
