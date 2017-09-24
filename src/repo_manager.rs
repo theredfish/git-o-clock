0d99481254fb0931ce576ac9c8ca42bbdf8f8c04
theredfish <did.julian@gmail.com>
2017-09-24 23:51:06 +0200

    Refactor repository model and add fn

--- src/repo_manager.rs
+++ src/repo_manager.rs
@@ -1,11 +1,7 @@
-use diesel;
-use diesel::prelude::*;
-use diesel::sqlite::SqliteConnection;
-use db::*;
 use glob::glob;
+use models::*;
+use std::fs;
 use std::result::Result;
-use std::path::PathBuf;
-use models::{Repository, NewRepository};
 
 #[derive(Debug)]
 pub struct GitRepo {
@@ -20,40 +16,52 @@ impl GitRepo {
     }
 
     pub fn add(&self, path: String) {
-        let search = path.to_string() + &self.pattern.to_string();
-        let repositories = self.search(search);
+        let pattern = path.to_string() + &self.pattern.to_string();
+        let repositories = self.search(pattern);
 
-        &self.create_repositories(&repositories);
+        println!("\nAdding your repositories...\n");
+
+        for new_repo in repositories {
+            let repo = create_repository(&new_repo);
+
+            // TODO : Handle unique contraint error and display it
+            // in a more user friendly way
+            // See https://github.com/diesel-rs/diesel/blob/73778af0827cb066bf1efdb348f380da14c916d4/diesel_tests/tests/errors.rs
+            match repo {
+                Ok(repo) => println!("-> {}", repo.name),
+                Err(err) => {
+                    println!("Can't save a repository {}", err);
+                    continue;
+                }
+            }
+        }
     }
 
     fn search(&self, pattern: String) -> Vec<NewRepository> {
         let mut repositories: Vec<NewRepository> = Vec::new();
 
+        println!("I'm searching into these repositories :\n");
+
         for path in glob(&pattern).unwrap().filter_map(Result::ok) {
-            let str_path = path.as_path().to_str().unwrap();
-            let parent = path.parent().unwrap();
+            let absolute_path = match fs::canonicalize(path) {
+                Ok(canonicalized_path) => canonicalized_path,
+                Err(_) => continue
+            };
+
+            let parent = absolute_path.parent().unwrap();
             let name = parent.iter().last().unwrap().to_str().unwrap();
-            let splitted_path: Vec<&str> = str_path.split(".git").collect();
-            let path = splitted_path[0];
+            let parent_path: Vec<&str> = parent.to_str().unwrap().split(".git").collect();
+            let path = parent_path[0];
+            println!("{:?}", path);
 
-            let new_repo = NewRepository::new(String::from(name), String::from(path));
+            let repo = NewRepository::new(
+                String::from(name),
+                String::from(path)
+            );
 
-            repositories.push(new_repo);
+            repositories.push(repo);
         }
 
         repositories
-    }
-
-    // -> Vec<Repository>
-    // TODO move to models
-    fn create_repositories<'a>(&self, new_repositories: &'a Vec<NewRepository>) -> usize {
-        use schema::repositories;
-
-        let connection = establish_connection();
-
-        diesel::insert(new_repositories)
-            .into(repositories::table)
-            .execute(&connection)
-            .expect("Error saving repositories")
     }
 }
