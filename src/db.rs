fc95232eee4185e7765a8b6822094aec25296868
theredfish <did.julian@gmail.com>
2017-12-05 16:13:11 +0100

    Auto generated database path variable

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,27 @@
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
-use dotenv::dotenv;
 use std::env;
+use std::process;
 
 pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
+    let exe_path = match env::current_exe() {
+        Ok(exe_path) => exe_path,
+        Err(e) => {
+            eprintln!("failed to get grm.exe path: {}", e);
+            process::exit(1);
+        }
+    };
+
+    let exe_dir = match exe_path.parent() {
+        Some(dir) => dir,
+        None => {
+            eprintln!("Failed to get grm.exe installation folder");
+            process::exit(1);
+        }
+    };
+
+    let key = "DATABASE_URL";
+    env::set_var(key, exe_dir.join("grm.sqlite3"));
 
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set");
