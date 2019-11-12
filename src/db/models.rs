fc491e6b2c393cf9bfebd28894f245fdb9ab8982
theredfish <did.julian@gmail.com>
2019-11-12 14:59:22 +0100

    Improve search errors from globwalk

--- src/db/models.rs
+++ src/db/models.rs
@@ -3,6 +3,8 @@ use crate::db::SqliteDatabase;
 use diesel;
 use diesel::prelude::*;
 use diesel::result::Error;
+use globwalk::DirEntry;
+use std::{convert::TryFrom, error, fmt};
 
 #[derive(Queryable, Debug)]
 pub struct Repository {
@@ -17,9 +19,55 @@ pub struct NewRepository {
     pub path: String,
 }
 
+#[derive(Debug)]
+pub enum NewRepositoryError {
+    Io(std::io::Error),
+    WalkPath,
+}
+
+impl fmt::Display for NewRepositoryError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "cannot create the repository for this path")
+    }
+}
+
+impl error::Error for NewRepositoryError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+        // Generic error, underlying cause isn't tracked.
+        None
+    }
+}
+
+impl From<std::io::Error> for NewRepositoryError {
+    fn from(err: std::io::Error) -> NewRepositoryError {
+        NewRepositoryError::Io(err)
+    }
+}
+
 impl NewRepository {
     pub fn new(name: String, path: String) -> NewRepository {
         NewRepository { name, path }
+    }
+}
+
+impl TryFrom<DirEntry> for NewRepository {
+    type Error = NewRepositoryError;
+
+    fn try_from(dir_entry: DirEntry) -> Result<Self, Self::Error> {
+        let dir_entry_path = dir_entry.path();
+        let absolute_path = dunce::canonicalize(&dir_entry_path)?;
+        let parent = absolute_path.parent().ok_or(NewRepositoryError::WalkPath)?;
+
+        let name = parent
+            .iter()
+            .last()
+            .ok_or(NewRepositoryError::WalkPath)?
+            .to_str()
+            .ok_or(NewRepositoryError::WalkPath)?;
+
+        let path = parent.to_str().ok_or(NewRepositoryError::WalkPath)?;
+
+        Ok(NewRepository::new(String::from(name), String::from(path)))
     }
 }
 
